aa96377035b357bba3ee19d8cb6fcf6be798c57a
V-donck <tv26363@gmail.com>
2022-03-09 12:39:34 +0100

    TCP sockets

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        GreetClient client = new GreetClient();
+        client.startConnection("127.0.0.1", 6666);
+        String response = client.sendMessage("hello server");
+        System.out.println(response);
     }
 }
