29cc12793d97ad156d27f4d8826b2c0f2b47ec68
Niels Van Gorp <vangorpniels@gmail.com>
2022-03-09 12:26:20 +0100

    Clean plate

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("thijs");
-        System.out.println("New feature");
-        System.out.println("New feature2");
-        System.out.println("Blablabla");
+
     }
 }
