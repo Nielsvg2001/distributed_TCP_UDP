169fe46ffd347a3feaf3aa3a7da45bd35b840f2d
Niels Van Gorp <vangorpniels@gmail.com>
2022-03-09 12:45:36 +0100

    werkt niet echt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+import java.io.*;
+import java.net.*;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
     }
 }
