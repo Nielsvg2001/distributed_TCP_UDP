bbea9191a3796408221bfb18f04d47a373ca4205
Niels Van Gorp <vangorpniels@gmail.com>
2022-03-16 18:28:39 +0100

    TCP multithread werkend en gecommit ;-)

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -11,32 +11,34 @@ public class Server {
 
 
         ServerSocket serverSocket = new ServerSocket(1234);
-        while (!serverSocket.isClosed()) {
-            try {
-                Socket socket = serverSocket.accept();
+        Thread thread = new Thread(() -> {
+            while (!serverSocket.isClosed()) {
+                try {
+                    Socket socket = serverSocket.accept();
 
-                DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                    DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
 
-                int fileNameLenght = dataInputStream.readInt();
-                if (fileNameLenght > 0) {
-                    byte[] fileNameBytes = new byte[fileNameLenght];
-                    dataInputStream.readFully(fileNameBytes, 0, fileNameBytes.length);
-                    String fileName = new String(fileNameBytes);
+                    int fileNameLenght = dataInputStream.readInt();
+                    if (fileNameLenght > 0) {
+                        byte[] fileNameBytes = new byte[fileNameLenght];
+                        dataInputStream.readFully(fileNameBytes, 0, fileNameBytes.length);
+                        String fileName = new String(fileNameBytes);
 
-                    int fileContentLenght = dataInputStream.readInt();
-                    if (fileContentLenght > 0) {
-                        byte[] fileContentBytes = new byte[fileContentLenght];
-                        dataInputStream.readFully(fileContentBytes, 0, fileContentBytes.length);
-                        File fileToDownload = new File(fileName);
-                        FileOutputStream fileOutputStream = new FileOutputStream(fileToDownload);
-                        fileOutputStream.write(fileContentBytes);
-                        fileOutputStream.close();
+                        int fileContentLenght = dataInputStream.readInt();
+                        if (fileContentLenght > 0) {
+                            byte[] fileContentBytes = new byte[fileContentLenght];
+                            dataInputStream.readFully(fileContentBytes, 0, fileContentBytes.length);
+                            File fileToDownload = new File(fileName);
+                            FileOutputStream fileOutputStream = new FileOutputStream(fileToDownload);
+                            fileOutputStream.write(fileContentBytes);
+                            fileOutputStream.close();
+                        }
                     }
+                } catch (IOException error) {
+                    error.printStackTrace();
                 }
-            } catch (IOException error) {
-                error.printStackTrace();
             }
-        }
-
+        });
+        thread.start();
     }
 }
