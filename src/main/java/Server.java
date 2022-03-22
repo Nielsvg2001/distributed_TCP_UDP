29d4d77027eca1b5cc04ea2d20c622553e9554fe
V-donck <tv26363@gmail.com>
2022-03-22 10:00:23 +0100

    Test new multithread

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -6,39 +6,40 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-
     public static void main(String[] args) throws IOException {
-
-
         ServerSocket serverSocket = new ServerSocket(1234);
-        Thread thread = new Thread(() -> {
+
             while (!serverSocket.isClosed()) {
-                try {
-                    Socket socket = serverSocket.accept();
-
-                    DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-
-                    int fileNameLenght = dataInputStream.readInt();
-                    if (fileNameLenght > 0) {
-                        byte[] fileNameBytes = new byte[fileNameLenght];
-                        dataInputStream.readFully(fileNameBytes, 0, fileNameBytes.length);
-                        String fileName = new String(fileNameBytes);
-
-                        int fileContentLenght = dataInputStream.readInt();
-                        if (fileContentLenght > 0) {
-                            byte[] fileContentBytes = new byte[fileContentLenght];
-                            dataInputStream.readFully(fileContentBytes, 0, fileContentBytes.length);
-                            File fileToDownload = new File(fileName);
-                            FileOutputStream fileOutputStream = new FileOutputStream(fileToDownload);
-                            fileOutputStream.write(fileContentBytes);
-                            fileOutputStream.close();
+                Socket socket = serverSocket.accept();
+                Thread thread = new Thread(() -> {
+                    try {
+                        while(!serverSocket.isClosed()) {
+                            System.out.println("accepted");
+                            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                            int fileNameLenght = dataInputStream.readInt();
+                            if (fileNameLenght > 0) {
+                                byte[] fileNameBytes = new byte[fileNameLenght];
+                                dataInputStream.readFully(fileNameBytes, 0, fileNameBytes.length);
+                                String fileName = new String(fileNameBytes);
+                                int fileContentLenght = dataInputStream.readInt();
+                                if (fileContentLenght > 0) {
+                                    byte[] fileContentBytes = new byte[fileContentLenght];
+                                    dataInputStream.readFully(fileContentBytes, 0, fileContentBytes.length);
+                                    File fileToDownload = new File(fileName);
+                                    FileOutputStream fileOutputStream = new FileOutputStream(fileToDownload);
+                                    fileOutputStream.write(fileContentBytes);
+                                    fileOutputStream.close();
+                                }
+                            }
                         }
+                    } catch (IOException error) {
+                        error.printStackTrace();
                     }
-                } catch (IOException error) {
-                    error.printStackTrace();
-                }
+                });
+
+
+                System.out.println("thred start");
+                thread.start();
             }
-        });
-        thread.start();
     }
 }
